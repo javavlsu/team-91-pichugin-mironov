3c45c9bb89510dadeeda6aa0f2d4d7e5124d6921
RetroZoomer <bayan-herov@mail.ru>
2023-07-15 22:40:58 +0300

    Добавлен: lombok, класс user, подключение к БД Изменён: pom.xml Приложение запускается

---
+++ src/main/java/ru/vlsu/ispi/entity/User.java
@@ -0,0 +1,24 @@
+package ru.vlsu.ispi.entity;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class User {
+    private String name;
+    private String lastName;
+    private String userDescription;
+    private String linkMes;
+    private LocalDate birthday;     // 2023-07-15 в таком формате
+    private Boolean pol;
+    private String login;
+    private String password;
+
+
+
+    public User(){
+
+    }
+
+}
